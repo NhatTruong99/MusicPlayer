7ca427c34d88780696401c9ace6103d26377ec02
truong <truong992001@gmail.com>
2021-08-28 16:47:02 +0700

    setLocalStogare

--- music.js
+++ music.js
@@ -2,6 +2,8 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+const PLAYER_STORAGE_KEY = 'player';
+
 const songContent = $('.song-content');
 const audio = $('audio');
 const playBtn = $('.btn-play');
@@ -12,13 +14,14 @@ const nextBtn =  $('.btn-next');
 const prevBtn = $('.btn-prev');
 const randomBtn =  $('.btn-random');
 const repeatBtn = $('.btn-repeat');
-
+const list =  $('.list');
 const app = {
     currentIndex: 0,
+    volumeValue: 1,
     isPlaying: false,
     isRandom: false,
     isRepeat: false,
-    isChangeProgress: false,
+    config: JSON.parse(localStorage.getItem(PLAYER_STORAGE_KEY))  || {},//vừa lấy và thêm
     songs: [
         {
             name: 'First date',
@@ -70,10 +73,19 @@ const app = {
             time: '1:53'
         }
     ],
+    setConfig: function(key,value){
+        app.config[key] = value;
+        localStorage.setItem(PLAYER_STORAGE_KEY, JSON.stringify(app.config));
+    },
+    loadConfig: function(){
+        this.isRandom = this.config.isRandom;
+        this.isRepeat = this.config.isRepeat;
+        this.volumeValue = this.config.volumeValue;
+    },
     render: function(){
         const htmls = this.songs.map((song,index) => {
             return `
-                <div class="card" value="${index}">
+                <div class="card" data-index="${index}">
                     <div class="card-avatar">
                         <img src="${song.image}" alt=""> 
                     </div>
@@ -87,10 +99,10 @@ const app = {
                 </div>
             `
         }).join('')
-        $('.list').innerHTML = htmls;
+        list.innerHTML = htmls;
     },
     handleEvent: function(){
-            audio.volume = 1;
+            audio.volume = this.volumeValue;
             volumeProgress.value = audio.volume * 100;
         //Xử lí nút play khi click
             playBtn.onclick = function(){
@@ -136,25 +148,42 @@ const app = {
             }
             progressLine.onmousedown = function(){
                 audio.pause();
-                app.isChangeProgress = !app.isChangeProgress;
             }
             progressLine.onmouseup = function(){
                 audio.play();
-                app.isChangeProgress = !app.isChangeProgress;
+            }
+            progressLine.ontouchstart = function(){
+                audio.pause();
+            }
+            progressLine.ontouchend = function(){
+                audio.play();
             }
             //Xử lí thay đổi âm lượng
             volumeProgress.oninput = function(){
-                audio.volume = volumeProgress.value /100;
+                audio.volume = volumeProgress.value / 100;
+                app.volumeValue = audio.volume;
+                app.setConfig('volumeValue',app.volumeValue);
             } 
+            //Xử lí khi click vào card trong list nhạc
+            list.onclick = function(e){
+                const songNode = e.target.closest('.card:not(.card-active)');
+                if (songNode){
+                    app.currentIndex = songNode.dataset.index;
+                    app.loadCurrentSong();
+                    app.changeCardActive();
+                    audio.play();
+                }
+            },
             //Xử lí nút next
             nextBtn.onclick = function(){
-                if (!app.isChangeProgress){}
                 if (app.isRandom){
                     app.playRandomSong()
                 } else{
                     app.nextSong();
                 }
+                app.changeCardActive();
                 audio.play();
+                app.scrollToActiveSong();
             }
             //Xử lí nút prev
             prevBtn.onclick = function(){
@@ -163,17 +192,21 @@ const app = {
                 } else{
                     app.prevSong();
                 }
+                app.changeCardActive();
                 audio.play();
+                app.scrollToActiveSong();
             }
             //Xử lí nút random
             randomBtn.onclick =function(){
                 app.isRandom = !app.isRandom;
+                app.setConfig('isRandom',app.isRandom);
                 randomBtn.classList.toggle('active', app.isRandom);
             }
             //Xử lí nút repeat
             repeatBtn.onclick =function(){
                 app.isRepeat = !app.isRepeat;
-                repeatBtn.classList.toggle('active', app.isRepeat)
+                app.setConfig('isRepeat',app.isRepeat);
+                repeatBtn.classList.toggle('active', app.isRepeat);
             }
             //Xử lí khi hết bài hát
             audio.onended = function(){
@@ -182,7 +215,6 @@ const app = {
                 } else{
                     nextBtn.click();
                 }
-
             }
     },
     defineProperties: function(){
@@ -199,6 +231,14 @@ const app = {
             <h3>${this.currentSong.name}</h3>
             <img src="${this.currentSong.image}" alt="">  
         `
+    },
+    scrollToActiveSong: function(){
+        setTimeout(() => {
+            $('.card-active').scrollIntoView({
+                behavior: 'smooth',
+                block: 'nearest'
+            })
+        },300)
     },
     nextSong: function(){
         this.currentIndex++;
@@ -222,8 +262,22 @@ const app = {
         app.currentIndex = newIndex;
         this.loadCurrentSong();
     },
-
+    changeCardActive: function(){
+        $$('.card').forEach( (card,index) => {
+            if (this.currentIndex == index){
+                card.classList.add('card-active');
+            } else {
+                card.classList.remove('card-active');
+            }
+        })
+    },
+    changeAppConfig: function(){
+        randomBtn.classList.toggle('active', app.isRandom);
+        repeatBtn.classList.toggle('active', app.isRepeat);
+    },
     start: function(){
+        //lấy giá trị từ local vào app
+        this.loadConfig();
         //Định nghĩa các thuộc tính cho object
         this.defineProperties();
         //tải thông tin bài hát vào giao diện load chính
@@ -232,7 +286,9 @@ const app = {
         this.handleEvent();
         //render playlist
         this.render();
-
+        this.changeCardActive();
+        //load trạng thái random, repeat
+        this.changeAppConfig();
     }
 }
 
